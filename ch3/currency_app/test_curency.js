403684abdb1550218ba4e1f4565ae7b886d648de
srikanth sanga <srikanth_sanga@intuit.com>
2014-11-21 00:03:24 -0800

    first commit

---
+++ ch3/currency_app/test_curency.js
@@ -0,0 +1,7 @@
+var currency = require('./currency');
+
+console.log('50 canadian dollars eaulas this amount of US dollars');
+console.log(currency.canadianToUS(50));
+
+console.log('30 US dollars equals this amount of canadian dollars:');
+console.log(currency.USToCanadian(30));
